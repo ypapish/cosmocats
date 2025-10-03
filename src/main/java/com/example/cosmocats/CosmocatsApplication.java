5485b621f1f812fb5b5bd3d5ab9c550dbfdf26e1
Yana <y.papish14@gmail.com>
2025-10-03 20:33:21 +0300

    Initial commit

---
+++ src/main/java/com/example/cosmocats/CosmocatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cosmocats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmocatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmocatsApplication.class, args);
+	}
+
+}
