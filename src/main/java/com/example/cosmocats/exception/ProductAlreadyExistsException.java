06e803ce509648bc857b9ac3cecadbed506813dd
Yana <y.papish14@gmail.com>
2025-10-03 20:46:04 +0300

    implement error handling

---
+++ src/main/java/com/example/cosmocats/exception/ProductAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.example.cosmocats.exception;
+
+
+public class ProductAlreadyExistsException extends RuntimeException {
+    public ProductAlreadyExistsException(String productName) {
+        super("Product already exists with name: " + productName);
+    }
+}
