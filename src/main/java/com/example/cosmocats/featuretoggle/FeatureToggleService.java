8d6b7858f855472cf60d8f34c7cc89c692b6e249
Yana <y.papish14@gmail.com>
2025-11-03 22:45:46 +0200

    add FeatureToggleService

---
+++ src/main/java/com/example/cosmocats/featuretoggle/FeatureToggleService.java
@@ -0,0 +1,27 @@
+package com.example.cosmocats.featuretoggle;
+
+import com.example.cosmocats.featuretoggle.config.FeatureToggleProperties;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeatureToggleService {
+
+    private final ConcurrentHashMap<String, Boolean> featureToggles;
+
+    public FeatureToggleService(FeatureToggleProperties featureToggleProperties) {
+        featureToggles = new ConcurrentHashMap<>(featureToggleProperties.getToggles());
+    }
+
+    public boolean check(String featureName) {
+        return featureToggles.getOrDefault(featureName, false);
+    }
+
+    public void enable(String featureName) {
+        featureToggles.put(featureName, true);
+    }
+
+    public void disable(String featureName) {
+        featureToggles.put(featureName, false);
+    }
+}
