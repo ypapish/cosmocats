2c8b1ead395c60a05c6a6d9a80ee115a70bc795f
Yana <y.papish14@gmail.com>
2025-11-03 22:52:07 +0200

    add annotation

---
+++ src/main/java/com/example/cosmocats/featuretoggle/annotation/FeatureToggle.java
@@ -0,0 +1,13 @@
+package com.example.cosmocats.featuretoggle.annotation;
+
+import com.example.cosmocats.featuretoggle.FeatureToggles;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface FeatureToggle {
+    FeatureToggles value();
+}
